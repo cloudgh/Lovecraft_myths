f9631d2a7fe73931832b519a6b583ba375905642
cloudgh <inastyaiv777@gmail.com>
2024-02-28 05:32:00 +0700

    init

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/Lovecraft_myths/'
+  : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
